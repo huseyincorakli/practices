f9e5fdb40482e40453d8caa5b5e59a7c2e271025
huseyin corakli <huseyincorakli@icloud.com>
2023-08-03 14:25:17 +0300

    ...-> ?todo dropzone

---
+++ 19/src/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import { useEffect } from 'react'
+import './App.css'
+import DropzoneComponent from './components/Dropzone'
+
+
+//https://localhost:7131/api/Product
+function App() {
+  
+
+  return (
+    <>
+      <div><DropzoneComponent/></div>
+    </>
+  )
+}
+
+export default App
