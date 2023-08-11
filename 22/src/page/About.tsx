56b249711b3504f7c4c5c06a54ab77ef78e1f674
huseyin corakli <huseyincorakli@icloud.com>
2023-08-11 13:53:55 +0300

    Redux

--- 22/src/page/About.tsx
+++ 22/src/page/About.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+interface User{
+  title:string;
+}
 
 const About = () => {
+  const [user, setUser] = useState<User[]>([])
+
+  useEffect(() => {
+    setTimeout(() => {}, 20000)
+    axios.get<User[]>('https://jsonplaceholder.typicode.com/photos').then(res => {
+      setUser(res.data)
+    })
+  }, [])
+
+ 
+
   return (
-    <div>About</div>
-  )
+  <div>
+    {user.length==0 ? <p>loading..</p> :user.map((user)=>(<p>{user.title}</p>))} 
+  </div>)
+
 }
 
 export default About
